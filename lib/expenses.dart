ce3fff77f655951cc0e0ea43edf0a83f4fcacd76
Arthurgt <arturbabkiewicz@gmail.com>
2023-08-02 01:35:43 +0200

    Commit_4 - Created ExpenseList widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/models/category.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
+import 'package:flutter_expense_tracker/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,13 +13,30 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _sampleExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Text('The chart'),
-          Text('Expenses list'),
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _sampleExpenses),
+          ),
         ],
       ),
     );
