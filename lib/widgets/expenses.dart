6412c1804225b98bd101cc161fb0066493e7e512
Arthurgt <arturbabkiewicz@gmail.com>
2023-08-02 02:25:03 +0200

    Commit_5 - Changed project structure

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/models/category.dart';
 import 'package:flutter_expense_tracker/models/expense.dart';
-import 'package:flutter_expense_tracker/expenses_list.dart';
+import 'package:flutter_expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_expense_tracker/expenses.dart';
+import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
