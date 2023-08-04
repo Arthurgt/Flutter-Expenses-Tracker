02b419c285ca8f6bb689c207c62de51c01eeb7c2
Arthurgt <arturbabkiewicz@gmail.com>
2023-08-04 21:47:57 +0200

    Commit_8 - Created controller for text value in user input

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,17 +10,36 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
-          children: const [
+          children: [
             TextField(
+              controller: _titleController,
               maxLength: 50,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 label: Text('Name'),
               ),
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                  },
+                  child: const Text('Save Expense'),
+                )
+              ],
             ),
           ],
         ));
